799c4c01bb2b5637a4953772582098ab197ea86b
letmetest2006-sami <letmetest2006@gmail.com>
2020-05-06 17:56:17 +0300

    update Login.java

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -5,8 +5,8 @@ public class HelloGitWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello new world again");
-        System.out.println("Hello again");
 
+        System.out.println("Hello again");
 
     }
 }
--- src/tests/Login.java
+++ src/tests/Login.java
@@ -2,10 +2,10 @@ package tests;
 public class Login {
 
     public static void main(String[] args) {
+
         System.out.println("Login");
+
         System.out.println("Log in again");
-
-
 
     }
 
